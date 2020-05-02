ca6ffadb9dac08bd0fb7bc4d54da5a762a50dde4
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-05-01 22:18:58 -0300

    EX CONSTRUCTOR FUNCTION

---
+++ JAVASCRIPT COMPLETO ES6/exercicios/construtor-function.js
@@ -0,0 +1,13 @@
+//funções construtoras são responsáveis por construir novos objetos sempre que chamamos ele
+const honda = new Carro("Honda", 3000)
+const fiat = new Carro("Fiat", 4000)
+
+function Carro(marca, precoInicial){
+    Carro.taxa = 1.2
+    const precoInicial = precoInicial * taxa
+    Carro.marca = marca
+    Carro.preco = precoFinal
+}
+
+const mazda = new Carro("mazda", 5000)
+console.log(mazda)
