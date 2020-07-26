cec9aea2dd2520b37282c25e22cb506e4cb03a51
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-07-26 17:26:01 -0300

    criando modal

---
+++ JAVASCRIPT COMPLETO ES6/projeto 1/js/modules/modal.js
@@ -0,0 +1,10 @@
+const initModal = () => {
+}
+
+const botaoAbrir = document.querySelector('[data-modal="abrir")');
+const botaoFechar = document.querySelector('[data-modal="fechar")');
+const containerModal = document.querySelector('[data-modal="container")');
+
+console.log(botaoAbrir)
+
+export default initModal;
--- JAVASCRIPT COMPLETO ES6/projeto 1/js/script-new.js
+++ JAVASCRIPT COMPLETO ES6/projeto 1/js/script-new.js
@@ -2,11 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import scrollAnimação from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabMenu from './modules/initTab.js';
-
-initAccordion()
-
-scrollAnimação();
+import initModal from './modules/modal.js';
 
 initScrollSuave();
-
+scrollAnimação();
+initAccordion()
 initTabMenu();
+initModal();
